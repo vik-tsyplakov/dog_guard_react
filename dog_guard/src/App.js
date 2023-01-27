dfc11dd07697e47a91592ee98002236bae5a9cf7
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-27 22:16:03 +0400

    connected-fonts

--- dog_guard/src/App.js
+++ dog_guard/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <p> fjfjfkgjfjgfhgijdigejge</p>
+      </header>
     </div>
   );
 }
